8ca66be4cbc4d508367e8665d5e5e3170a40736f
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-06-23 13:15:09 -0500

    add apis and a module
    Co-authored-by: mikeflintrop <mikeflintrop@users.noreply.github.com>
    Co-authored-by: diegob12@live.com <diegob12@live.com>
    Co-authored-by: Mai Hlee (Miley) Vang <MileyLuna@users.noreply.github.com>
    Co-authored-by: Miski <MiskiAli@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1,17 +1,37 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
+const players = require('./modules/players.js');
 
 // This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
 
+app.get('/player-data', (req, res) => {
+	// on the url /player-data, send the player data
+	console.log(players);
+	res.send(players);
+});
 
-app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
-})
+app.post('/player-data', (req, res) => {
+	// the quote
+	console.log(req.body);
+
+	// send back response
+	res.sendStatus(201);
+});
+
+// generate random number function
+// if conditional to check each guess to the random number
+// store previous guess
+// store each player as an array with an object
+// get data from client
+
+app.listen(PORT, function () {
+	console.log('SERVER RUNNING ON PORT', PORT);
+});
